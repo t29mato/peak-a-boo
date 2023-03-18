c59970be2ed3bf5f8ee8ab9ccd13ebdf4e31e0f7
t29mato <tomoya.matou@gmail.com>
2023-03-19 06:44:09 +0900

    vue-analyticsライブラリで計測できないので試しにgoogle-analyticsのUI上で発行されるjsタグを設置してみる

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RKW409255W"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', process.env.VUE_APP_GOOGLE_ANALYTICS_TRACKING_ID);
+</script>
+
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
