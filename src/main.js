186857418651e92384f02566917b90e9b71c31ab
t29mato <tomoya.matou@gmail.com>
2023-03-18 22:55:17 +0900

    Google Analyticsを設置

--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 import * as VueGoogleMaps from 'vue2-google-maps';
+import VueAnalytics from 'vue-analytics'
 
 Vue.use(VueGoogleMaps, {
   load: {
     key: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
     libraries: 'places', // This is required if you use the Autocomplete plugin
   }
+})
+
+Vue.use(VueAnalytics, {
+  id: process.env.VUE_APP_GOOGLE_ANALYTICS_TRACKING_ID,
+  checkDuplicatedScript: true
 })
 
 Vue.component('google-map', VueGoogleMaps.Map);
