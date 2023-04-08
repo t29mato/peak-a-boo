7adb2d8a5a46641f2066439ee1e0af0830bccc08
t29mato <tomoya.matou@gmail.com>
2023-04-08 22:55:33 +0900

    fix: vue-analytics has already been deprecated

--- src/main.js
+++ src/main.js
@@ -4,18 +4,17 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 import * as VueGoogleMaps from 'vue2-google-maps';
-import VueAnalytics from 'vue-analytics'
+import VueGtag from "vue-gtag";
+
+Vue.use(VueGtag, {
+  config: { id: process.env.VUE_APP_GOOGLE_ANALYTICS_TRACKING_ID }
+});
 
 Vue.use(VueGoogleMaps, {
   load: {
     key: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
     libraries: 'places', // This is required if you use the Autocomplete plugin
   }
-})
-
-Vue.use(VueAnalytics, {
-  id: process.env.VUE_APP_GOOGLE_ANALYTICS_TRACKING_ID,
-  checkDuplicatedScript: true
 })
 
 Vue.component('google-map', VueGoogleMaps.Map);
